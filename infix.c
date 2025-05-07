29e166bfd4843649e5ea471930bd54596e144246
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-05-07 21:10:44 +0530

    last string declare

--- infix.c
+++ infix.c
@@ -1,2 +1,26 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include <stdbool.h>
+#define MAX 100
 
+bool isCharacter(char);
+bool isoperator(char);
+bool ignorable(char);
+
+int main(void) {
+    char* input = malloc(sizeof(char) * MAX);
+    
+    return 0;
+}
+
+bool isCharacter(char c) {
+    return (c > 64 && c < 91) || (c > 96 && c < 123);
+}
+
+bool isoperator(char c){
+    return (c == '+' || c=='-'|| c=='*'|| c=='/' || c=='^');
+}
+
+bool ignorable(char c) {
+    return c == ' ' || c == '\n' || c == '\t';
+}
